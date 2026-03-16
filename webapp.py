54909af094d6b9589f4f95d39e615f97882ae0df
SAHILPRAJAPATI <sp7940265@gmail.com>
2026-03-16 08:43:24 +0530

    Update slider labels and model loading syntax
    Refactor slider labels and streamline code.

--- webapp.py
+++ webapp.py
@@ -1,15 +1,10 @@
-import streamlit as st
+mport streamlit as st
+st.title("Iris Prediction")
 import pickle
-
-st.title("Iris Prediction App")
-
-model = pickle.load(open("model_svm.pkl", "rb"))
-
-sl = st.slider("Sepal Length", 2.0, 10.0)
-sw = st.slider("Sepal Width", 2.0, 10.0)
-pl = st.slider("Petal Length", 2.0, 10.0)
-pw = st.slider("Petal Width", 2.0, 10.0)
-
+model=pickle.load(open("model_svm.pkl","rb"))
+sl=st.slider("Sl",2.0,10.0)
+sw=st.slider("Sw",2.0,10.0)
+pl=st.slider("pl",2.0,10.0)
+pw=st.slider("pw",2.0,10.0)
 if st.button("Predict"):
-    prediction = model.predict([[sl, sw, pl, pw]])
-    st.success(prediction)
+    st.success(model.predict([[sl,sw,pl,pw]]))
